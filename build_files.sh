3675045cbf462f5cf270003cd864ddba237146ee
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-29 20:10:59 +0100

    another method with asgi.py file

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-python3.12 -m venv venv
+# Create virtual environment
+python3.9 -m venv venv
 source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
