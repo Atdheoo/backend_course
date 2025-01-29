1270c4d506a74075f7170eb5a215cade844fac06
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-29 20:07:38 +0100

    update root files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
-echo "BUILD START"
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
-echo "BUILD COMPLETED"
+#!/bin/bash
+
+python3.12 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
