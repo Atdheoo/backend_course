80d2373180289bf0e76d77493b8deb88c68eb6e3
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-29 20:15:19 +0100

    update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
-#!/bin/bash
-
-python3.9 -m venv venv
-source venv/bin/activate
+echo "Build Start"
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
-
+python manage.py collectstatic --noinput --clear
+echo "Build End"
