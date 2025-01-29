e61efe70abf72d60bd275ddac9fde9acd9154a52
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-29 20:13:03 +0100

    inew update

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Create virtual environment
 python3.9 -m venv venv
 source venv/bin/activate
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --noinput
+
