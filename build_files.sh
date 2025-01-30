e4eca9aa940e18b9b3bbb9350223b1fe1e08657d
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-30 03:45:05 +0100

    new build files update

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Ensure a clean build environment
-rm -rf staticfiles_build  # Remove old build if exists
-mkdir -p staticfiles_build  # Ensure directory exists
+rm -rf staticfiles_build
+mkdir -p staticfiles_build
 
-# Install dependencies
-pip install -r requirements.txt
+# Explicitly use Python 3.9
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput --clear --verbosity 3
+python3.9 manage.py collectstatic --noinput --clear --verbosity 3
 
 # Move collected static files to the correct directory
 mv staticfiles staticfiles_build
