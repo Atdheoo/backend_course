571e6af6a8636d447f9cf0b9467fd0ec7d859ffb
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-30 03:46:22 +0100

    new up

--- build_files.sh
+++ build_files.sh
@@ -5,11 +5,11 @@ rm -rf staticfiles_build
 mkdir -p staticfiles_build
 
 # Explicitly use Python 3.9
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput --clear --verbosity 3
+python3 manage.py collectstatic --noinput --clear --verbosity 3
 
 # Move collected static files to the correct directory
 mv staticfiles staticfiles_build
