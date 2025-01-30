78c9c710c8baf34cf38528c71e19bd90d2963353
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-30 03:49:17 +0100

    udaptede build files

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-
-# Ensure a clean build environment
-rm -rf staticfiles_build
-mkdir -p staticfiles_build
-
-# Explicitly use Python 3.9
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
-
-# Collect static files
-python3 manage.py collectstatic --noinput --clear --verbosity 3
-
-# Move collected static files to the correct directory
-mv staticfiles staticfiles_build
+echo "Build Start"
+python3 -m pip install requirements.txt
+python3 manage.py collectstatic --noinput --clear
+echo "Build End"
