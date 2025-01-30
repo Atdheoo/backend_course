cf962833a4876c429da7151cf5c0384049534379
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-30 04:24:41 +0100

    add db

--- backend/settings.py
+++ backend/settings.py
@@ -79,11 +79,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backend',
+        'USER': 'backenduser',
+        'PASSWORD': 'backendpassword',
+        'HOST': 'localhost',  # Example: 'db.supabase.co'
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
