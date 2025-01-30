ccb1ae3f1a33de63437c69174635d8a8f04a9025
Atdheoo <atdhe.muharremi@gmail.com>
2025-01-30 03:41:51 +0100

    new updates

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
-echo "Build Start"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "Build End"
+#!/bin/bash
+
+# Ensure a clean build environment
+rm -rf staticfiles_build  # Remove old build if exists
+mkdir -p staticfiles_build  # Ensure directory exists
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput --clear --verbosity 3
+
+# Move collected static files to the correct directory
+mv staticfiles staticfiles_build
